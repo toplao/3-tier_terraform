c2ed1ba6a49ab7eaff58c6b98051147fd24f4060
Ola Ayilegbe <toplao@yahoo.com>
2023-04-18 17:29:31 +0000

    Added new terraform reference files

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -10,27 +10,27 @@ output "vpc_id" {
     value = aws_vpc.vpc.id
 }
 
-output "public_subnet_az1" {
+output "public_subnet_az1_id" {
     value = aws_subnet.public_subnet_az1.id
 }
 
-output "public_subnet_az2" {
+output "public_subnet_az2_id" {
     value = aws_subnet.public_subnet_az2.id
 }
 
-output "private_app_subnet_az1" {
+output "private_app_subnet_az1_id" {
     value = aws_subnet.private_app_subnet_az1.id
 }
 
-output "private_app_subnet_az2" {
+output "private_app_subnet_az2_id" {
     value = aws_subnet.private_app_subnet_az2.id
 }
 
-output "private_data_subnet_az1" {
+output "private_data_subnet_az1_id" {
     value = aws_subnet.private_data_subnet_az1.id
 }
 
-output "private_data_subnet_az2" {
+output "private_data_subnet_az2_id" {
     value = aws_subnet.private_data_subnet_az2.id
 }
 
