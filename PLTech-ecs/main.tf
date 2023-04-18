326944cac06c158709dcfecf26a379150d18b5cc
Ola Ayilegbe <toplao@yahoo.com>
2023-04-18 21:06:09 +0000

    Security Group Module added

--- PLTech-ecs/main.tf
+++ PLTech-ecs/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
     profile = "default"
 }
 
-# create vpc
+# create vpc module
 module "vpc" {
     source                          = "../modules/vpc"
     region                          = var.region
@@ -18,7 +18,7 @@ module "vpc" {
     private_data_subnet_az2_cidr    = var.private_data_subnet_az2_cidr
 }
 
-# create nat gateways
+# create nat gateways module
 module "nat_gateway" {
     source                          = "../modules/nat-gateway"
     public_subnet_az1_id            = module.vpc.public_subnet_az1_id
@@ -30,4 +30,9 @@ module "nat_gateway" {
     private_app_subnet_az2_id       = module.vpc.private_app_subnet_az2_id
     private_data_subnet_az2_id      = module.vpc.private_data_subnet_az2_id
 }
-#added.
+
+#create security groups module
+module "security_group" {
+    source = "../modules/security-groups"
+    vpc_id = module.vpc.vpc_id
+}
