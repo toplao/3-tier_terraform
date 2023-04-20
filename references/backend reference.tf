b8052ac352e1c7fe843d742ba1f50f1c96c32e89
Ola Ayilegbe <toplao@yahoo.com>
2023-04-20 16:24:42 +0000

    added other reference files

--- references/backend reference.tf
+++ references/backend reference.tf
@@ -1,9 +1,10 @@
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = 
-    key       = 
-    region    = 
-    profile   = 
+    bucket         = 
+    key            = 
+    region         = 
+    profile        = 
+    dynamodb_table = 
   }
 }
