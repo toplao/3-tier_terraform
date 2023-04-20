452e12074e73bc9a1e0499eee0c54b02705c5eeb
Ola Ayilegbe <toplao@yahoo.com>
2023-04-20 15:45:56 +0000

    added awscertmgr module

--- PLTech-ecs/main.tf
+++ PLTech-ecs/main.tf
@@ -37,15 +37,26 @@ module "security_group" {
     vpc_id = module.vpc.vpc_id
 }
 
-# create IAM role
+# create IAM role module
 module "ecs_task_execution_role" {
     source          = "../modules/ecs-task-execution-role"
     project_name    = module.vpc.project_name
 }
 
-# create acm
+# create acm (aws certificate manager) module
 module "acm" {
   source            = "../modules/awscertmgr"
   domain_name       = var.domain_name
   alternative_name  = var.alternative_name
+}
+
+# create alb (application load balancer) module
+module "application_load_balancer" {
+  source                    = "../modules/alb"
+  project_name              = module.vpc.project_name
+  alb_security_group_id     = module.security_group.alb_security_group_id
+  public_subnet_az1_id      = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id      = module.vpc.public_subnet_az2_id
+  vpc_id                    = module.vpc.vpc_id
+  certificate_arn           = module.acm.certificate_arn
 }
